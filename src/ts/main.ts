6e730511aea29b7725595365bbede1539258caf4
JAG24_Exo <joseguillen587@yahoo.com>
2024-10-02 16:17:34 -0700

    imported bootstrap icons and fontawesome

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,5 +1,12 @@
+// Imports for bootstrap and popper.js
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap';
+
+// Imports for fontawesome and bootstrap-icons
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+
+// Custom CSS
 import '../css/style.min.css';
 
 // Bootstrap breakpoint css
