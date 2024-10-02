805fa4a3d8bd5e4e666502eddef6de20ec3829c0
JAG24_Exo <joseguillen587@yahoo.com>
2024-10-02 16:00:20 -0700

    moved copyright year code to bottom of main.ts

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,14 +1,37 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap';
 import '../css/style.min.css';
-
 import { updateCopyrightYear } from './copyright-update';
 
-// Call the helper function
-document.addEventListener('DOMContentLoaded', () => {
-    updateCopyrightYear();
-});
+
 
 
 
 console.log(`Hello World! Is TypeScript working?`);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// helper function to update the year in the footer
+document.addEventListener('DOMContentLoaded', () => {
+    updateCopyrightYear();
+});
