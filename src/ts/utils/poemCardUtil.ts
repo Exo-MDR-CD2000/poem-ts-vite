ffa962d7366469bc2d785fc0a7d2e71bdd5133c8
JAG24_Exo <joseguillen587@yahoo.com>
2024-10-04 20:00:36 -0700

    fixed delete button styling by adding unique id to it

--- src/ts/utils/poemCardUtil.ts
+++ src/ts/utils/poemCardUtil.ts
@@ -12,7 +12,7 @@ export const createSmallPoemCard = (poem: Poem): HTMLElement => {
                 <h6 class="card-subtitle mb-2 text-muted">${poem.author}</h6>
                 <p class="card-text">Line Count: ${poem.linecount}</p>
                 <p class="card-text">${poem.lines.join('<br>')}</p>
-                <button class="btn btn-danger btn-sm position-absolute top-0 end-0 m-2 delete-poem" data-id="${poem.id}">
+                <button class="btn btn-danger btn-sm position-absolute top-0 end-0 m-2 delete-poem-random" data-id="${poem.id}">
                     <i class="bi bi-trash"></i>
                 </button>
             </div>
@@ -32,7 +32,7 @@ export const createLargePoemCard = (poem: Poem): HTMLElement => {
                 <h6 class="card-subtitle mb-2 text-muted">${poem.author}</h6>
                 <p class="card-text">Line Count: ${poem.linecount}</p>
                 <p class="card-text">${poem.lines.join('<br>')}</p>
-                <button class="btn btn-danger btn-lg-1 position-absolute top-0 end-0 m-2 delete-poem" data-id="${poem.id}">
+                <button class="btn btn-danger btn-lg-1 position-absolute top-0 end-0 m-2 delete-poem-random" data-id="${poem.id}">
                     <i class="bi bi-trash"></i>
                 </button>
             </div>
